08d8cbd93e9b1787c380cf01ffec28e2fac2de79
Jaider Perez <jaider.dperez@outlook.com>
2023-02-06 01:13:55 -0600

    Tarea completa

--- productos.js
+++ productos.js
@@ -1,9 +1,23 @@
-const { parse } = require("path")
-const { listenerCount } = require("process")
-
-const express = requiere('express')
-const product = JSON.parse(productos)
-
+const { response } = require('express')
+const express = require('express')
 const app = express()
+const fs = require('fs')
+const { stringify } = require('querystring')
+let productos = fs.readFileSync('./productos.json', 'utf-8')
+var obj = JSON.parse(productos)
 
 app.listen(8080, () => console.log('Server en linea'))
+
+app.get('/productos/:id', (request, response) => {
+    const { id } = request.params
+    var product = obj.find(item => item.id == id)
+    if(!product) response.send({ error: "Id no encontrado"})
+    else response.send({ product })
+})
+
+app.get('/productos', (req, res) => {
+    const {limit} = req.query
+    if(limit == 0 || !limit) res.send({obj})
+    else res.send(obj.slice(0,limit))
+})
+
